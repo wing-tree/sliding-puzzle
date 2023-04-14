e040c6c273bce10c425073a9445b04203e5ce976
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-04-14 23:54:40 +0900

    Add safe logic

--- app/src/main/java/com/wing/tree/sid/sliding/puzzle/extension/TextView.kt
+++ app/src/main/java/com/wing/tree/sid/sliding/puzzle/extension/TextView.kt
@@ -1,25 +1,25 @@
 package com.wing.tree.sid.sliding.puzzle.extension
 
 import android.widget.TextView
-import androidx.annotation.StringRes
 import com.wing.tree.sid.core.extension.negative
+import com.wing.tree.sid.core.extension.not
 import kotlin.math.roundToInt
 
 fun TextView.setDongleText(text: CharSequence) {
     this.text = text
 
-    val fontMetrics = paint.fontMetrics
-    val ascent = fontMetrics.ascent
-    val top = fontMetrics.top
+    if (tag.not(true)) {
+        val fontMetrics = paint.fontMetrics
+        val ascent = fontMetrics.ascent
+        val top = fontMetrics.top
 
-    setPadding(
-        paddingLeft,
-        paddingTop.plus(ascent.minus(top).roundToInt().negative),
-        paddingRight,
-        paddingBottom
-    )
-}
+        setPadding(
+            paddingLeft,
+            paddingTop.plus(ascent.minus(top).roundToInt().negative),
+            paddingRight,
+            paddingBottom
+        )
+    }
 
-fun TextView.setDongleText(@StringRes resid: Int) {
-    setDongleText(resources.getString(resid))
+    tag = true
 }
