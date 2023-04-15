8d6c15d488548d1a52e31dd00a77f0419d316f2e
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-04-15 20:37:55 +0900

    Define Fragment extension properties

--- app/src/main/java/com/wing/tree/sid/sliding/puzzle/extension/Fragment.kt
+++ app/src/main/java/com/wing/tree/sid/sliding/puzzle/extension/Fragment.kt
@@ -4,4 +4,8 @@ import androidx.annotation.DimenRes
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 
+val Fragment.configShortAnimTime: Int get() = resources.getInteger(android.R.integer.config_shortAnimTime)
+val Fragment.configMediumAnimTime: Int get() = resources.getInteger(android.R.integer.config_mediumAnimTime)
+val Fragment.configLongAnimTime: Int get() = resources.getInteger(android.R.integer.config_longAnimTime)
+
 fun Fragment.getFloat(@DimenRes id: Int) = ResourcesCompat.getFloat(resources, id)
